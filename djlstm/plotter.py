19c36fb9be19a3efe38195dcbb6165a13d0a9774
Callan Hoskins <callanhoskins@gmail.com>
2019-03-15 01:44:37 -0700

    Model 5 has plots now!

--- djlstm/plotter.py
+++ djlstm/plotter.py
@@ -1,38 +1,63 @@
 import matplotlib.pyplot as plt
+import os
 
 '''
 This file is all about the GRAPHS
 '''
 
-# Pass in history, returned by call to model.fit()
-def display_all_plots(history, deeper, wider, dropout, 
-	learning_rate):
-	plot_name = 'deeper={}_wider={}_dropout={}_lr={}'.format(
-		deeper, wider, dropout, learning_rate)
-	plot_loss(history, plot_name)
-	plot_accuracy(history, plot_name)
-
-def plot_loss(history, plot_name):
-	# summarize history for loss
-	plt.plot(history.history['loss'])
-	plt.plot(history.history['val_loss'])
-	title = "LOSS_" + plot_name
+# Creates a plot of the training history of each model
+# with respect to metric
+# @param metric: string metric id (e.g. 'acc', 'loss', etc)
+# @return none
+# Called by display_all_plots
+def plot_metric(metric, histories, legend,
+	destination_folder = ""):
+	# summarize history for metric
+	print('called plot_metric with metric' + metric)
+	title = metric.upper()
+	print(title)
+	for hist_tuple in histories:
+		num_epochs = len(hist_tuple[0].history[metric])
+		print('len of epoch_count: ' + str(len(hist_tuple[0].history[metric])))
+		print('len of '+metric+': ' + str(len(hist_tuple[0].history[metric])))
+		plt.plot(range(1,num_epochs+1), hist_tuple[0].history[metric])
 	plt.title(title)
-	plt.ylabel('loss')
+	plt.ylabel(metric)
 	plt.xlabel('epoch')
-	plt.legend(['train', 'test'], loc='upper left')
+	plt.legend(legend, loc='upper left')
 	plt.show()
-	plt.savefig(title+'.png')
+	plt.savefig(destination_folder + title+'.png')
+	plt.clf()
 
+# Plots models' performances based on given metrics
+# @param histories: list of tuples of the form 
+#	(history, title)
+# where history is a History object returned by model.fit()
+# and title is a string identifier of that model
+# @param metrics: list of string metrics to be shown
+# @param name of the folder where 
+def display_all_model_plots(histories, folder_name = ""):
+	if not folder_name == "" and not os.path.isdir(folder_name):
+		os.mkdir(folder_name)
+	if not folder_name == "":
+		folder_name += "/"
+	legend = []
+	metrics = list(histories[0][0].history.keys())
+	for hist_tuple in histories:
+		legend.append(hist_tuple[1])
+	for metric in metrics: 
+		plot_metric(metric, histories, legend,
+			destination_folder = folder_name)
 
-def plot_accuracy(history, plot_name):
-	# summarize history for accuracy
-	plt.plot(history.history['acc'])
-	plt.plot(history.history['val_acc'])
-	title = "ACCURACY_" + plot_name
-	plt.title('title')
-	plt.ylabel('accuracy')
-	plt.xlabel('epoch')
-	plt.legend(['train', 'test'], loc='upper left')
-	plt.show()
-	plt.savefig(title+'.png')
+'''
+TO INCORPORATE PLOTTER (display_all_model_plots) INTO OUR ML MODEL:
+- create an empty list before parameter search
+- every time parameter search fits a new model, create a title
+	specifying the model created, then append that as well as the 
+	History object returned by model.fit as a tuple to the end 
+	of your list
+	(e.g. title = "Deeper={}, Wider={}, LR={}, Dropout={}".format(
+                    deeper,wider,learning_rate,dropout))
+    list.append((model.fit(....), title))
+- after parameter search is done, pass the list of tuples to display_all_model_plots
+'''
